97f4366cacfed86f7be384cdd4824be695896f6d
diarise <diarise@users.noreply.github.com>
2025-09-30 19:48:43 -0700

    Update generate_taxonomies.rb

--- _scripts/generate_taxonomies.rb
+++ _scripts/generate_taxonomies.rb
@@ -63,3 +63,60 @@ tags.each do |tag|\
 end\
 \
 puts "\uc0\u9989  Generated #\{categories.size\} categories and #\{tags.size\} tags."}
+require 'yaml'
+require 'fileutils'
+
+# Paths
+categories_dir = "_categories"
+tags_dir = "_tags"
+
+# Ensure dirs exist
+FileUtils.mkdir_p(categories_dir)
+FileUtils.mkdir_p(tags_dir)
+
+# Collect categories and tags
+categories = []
+tags = []
+
+Dir["_posts/*"].each do |post_file|
+  content = File.read(post_file)
+  if content =~ /^---(.+?)---/m
+    front_matter = YAML.safe_load($1)
+    categories.concat(Array(front_matter["categories"])) if front_matter["categories"]
+    tags.concat(Array(front_matter["tags"])) if front_matter["tags"]
+  end
+end
+
+# Deduplicate
+categories.uniq!
+tags.uniq!
+
+# Generate category pages
+categories.each do |cat|
+  filename = "#{categories_dir}/#{cat.downcase.gsub(" ", "-")}.md"
+  File.open(filename, "w") do |f|
+    f.puts <<~HEREDOC
+    ---
+    layout: category
+    title: "#{cat}"
+    permalink: /categories/#{cat.downcase.gsub(" ", "-")}/
+    ---
+    HEREDOC
+  end
+end
+
+# Generate tag pages
+tags.each do |tag|
+  filename = "#{tags_dir}/#{tag.downcase.gsub(" ", "-")}.md"
+  File.open(filename, "w") do |f|
+    f.puts <<~HEREDOC
+    ---
+    layout: tag
+    title: "#{tag}"
+    permalink: /tags/#{tag.downcase.gsub(" ", "-")}/
+    ---
+    HEREDOC
+  end
+end
+
+puts "✅ Generated #{categories.size} categories and #{tags.size} tags."
